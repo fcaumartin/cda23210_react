63eda1440abe3547f959834470c87c57783ee429
frc <fcaumartin@gmail.com>
2024-02-29 14:50:31 +0100

    form post

--- api/src/Form.jsx
+++ api/src/Form.jsx
@@ -1,12 +1,45 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Form() {
 
+  const [libelle, setLibelle] = useState("");
+  const [description, setDescription] = useState("");
+  const [prix, setPrix] = useState("");
+
   const navigate = useNavigate();
 
+  function handleChangeLibelle(evt) {
+    setLibelle(evt.target.value)
+  }
 
-  function handleClick() {
+  function handleChangeDescription(evt) {
+    setDescription(evt.target.value)
+  }
+
+  function handleChangePrix(evt) {
+    setPrix(evt.target.value)
+  }
+
+
+  function handleClickValider() {
+
+    axios.post("http://127.0.0.1:8000/api/produits", 
+      {
+        libelle: libelle,
+        description: description,
+        prix: prix,
+        image: "",
+        sousCategorie: "/api/sous_categories/1"
+      }
+    ).then(()=>{
+
+      navigate("/list", { replace: true });
+    })
+  }
+
+  function handleClickAnnuler() {
 
     navigate("/list", { replace: true });
   }
@@ -20,7 +53,12 @@ function Form() {
       <h1>Formulaire</h1>
       <hr />
       
-      <button onClick={handleClick}>Valider</button>
+      <input onChange={handleChangeLibelle} value={libelle} type="text" placeholder="libelle"/><br/>
+      <input onChange={handleChangeDescription} value={description} type="text" placeholder="description"/><br/>
+      <input onChange={handleChangePrix} value={prix} type="text" placeholder="prix"/><br/>
+
+      <button onClick={handleClickValider}>Valider</button>
+      <button onClick={handleClickAnnuler}>Annuler</button>
     </div>
   );
 }
